20efa56e67e247a31ac512c2bd0a85c153842850
Jacob Williams <JaWilliamsDev@gmail.com>
2019-07-10 20:53:28 -0400

    Crated Perosn Class in lambda-classes

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -1,1 +1,13 @@
 // CODE here for your Lambda Classes
+class Person{
+    constructor(attb){
+        this.name = attb.name;
+        this.age = attb.age;
+        this.location = attb.location;
+    }
+
+    speak() {
+        console.log(`Hello my name is ${this.name}, I am from ${this.location}.`)
+    };
+
+}
